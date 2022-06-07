af258035b6d4eebca3eba1baa4cbe4c469e282fe
Noble-Chain <sawchain17@gmail.com>
2022-06-07 09:55:50 +0630

    starter version

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+@extends('master')
+@section('title') Home @endsection
+
+@section('content')
+
+<div class="container">
+    <div class="row my-5 justify-content-center">
+        <div class="col-lg-8">
+            <div class="d-flex justify-content-between">
+                <h1>Laravel CRUD</h1>
+                <div class="d-flex align-items-center">
+                    <a class=" btn btn-lg" href="">User</a>
+                    <a href="" class="btn btn-lg">Create</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
